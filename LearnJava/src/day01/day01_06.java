0ab89ec98b048125e24f0e5b8163ad8fb45057f4
XiaoSa <sxiao@stu.xidian.edu.cn>
2017-03-15 19:25:41 +0800

    day1
    1

---
+++ LearnJava/src/day01/day01_06.java
@@ -0,0 +1,14 @@
+package day01;
+//标识符
+public class day01_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		定义合法的标识符：
+//		1、不可以数字开头
+//		2、不可以关键字开头
+		
+
+	}
+
+}
